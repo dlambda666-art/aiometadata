726975b8a10fbc8b98b656aaf8bbf659c9ef3f3c
dlambda666-art <d.lambda666@gmail.com>
2026-09-16 15:05:14 +0200

    fix(hf): restore HOST_NAME from Space host

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -e
 
+# Hugging Face Spaces provides SPACE_HOST. AIOMetadata expects HOST_NAME.
+if [ -z "${HOST_NAME:-}" ] && [ -n "${SPACE_HOST:-}" ]; then
+  export HOST_NAME="https://${SPACE_HOST}"
+fi
+
 exec node dist/server/server.js
